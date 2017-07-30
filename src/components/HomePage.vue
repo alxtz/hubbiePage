e4f34842ba49f86eb68f1b149b2d743e1eb6bc42
Alex Tzeng <alxtz.tw@gmail.com>
2017-07-30 21:08:39 +0800

    Hubie production

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="root">
     <div class="container-fluid">
-      <h1>Hubbie</h1>
+      <h1 class="name">Hubbie</h1>
+      <input type="text" class="search" placeholder="Please Enter Your Code">
+      <ul class="cates">
+        <li class="cate">Product Design</li>
+        <li class="cate">Motion Graphic</li>
+        <li class="cate">UI</li>
+        <li class="cate">Photography</li>
+      </ul>
+
+      <a class="enter" href="#">Enter</a>
 
     </div>
     <div class="background">
@@ -56,6 +65,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+  @import url('https://fonts.googleapis.com/css?family=Roboto');
+  @import url('https://fonts.googleapis.com/css?family=Play');
+  @import url('https://fonts.googleapis.com/css?family=Anton');
+
   .root {
     width: 100vw;
     height: 100vh;
@@ -64,7 +77,83 @@ export default {
     height: 100vh;
     width: 100vw;
     position: absolute;
-    display: none;
+    background-color: rgba(0,0,0,0.55);
+    z-index: 10;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h1.name {
+      color: #ffb300;
+      font-family: 'Anton';
+      font-weight: 400;
+      text-transform: uppercase;
+      font-size: 100pt;
+      line-height: 15vh;
+      letter-spacing: 1vh;
+      margin-top: 20vh;
+      margin-bottom: 15vh;
+      text-align: center;
+      padding-left: 15pt;
+    }
+
+    input.search {
+      width: 350pt;
+      margin: 0 auto;
+      border-radius: 4px;
+      border: none;
+      box-shadow: 2px 4px 4px 0 rgba(0, 0, 0, 0.5);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      line-height: 29pt;
+      height: 29pt;
+      font-size: 18pt;
+      font-family: 'play';
+      text-align: center;
+      font-weight: 600;
+    }
+    input.search::placeholder {
+      padding-top: 2pt;
+      text-align: center;
+      font-family: 'play';
+      color: #969696;
+      font-size: 18pt;
+      font-weight: 600;
+    }
+    input:focus {
+      outline: none;
+    }
+    input:focus::placeholder {
+      opacity: 0;
+    }
+
+    ul.cates {
+      display: flex;
+      width: 450pt;
+      margin: 0 auto;
+      justify-content: space-between;
+      padding-left: 0;
+      margin-top: calc(5pt + 1.5vh);
+      li.cate {
+        list-style-type: none;
+        font-family: 'play';
+        color: #d6d6d6;
+        margin-left: 0;
+        font-size: 17pt;
+      }
+    }
+
+    a.enter
+    {
+      font-family: 'play';
+      text-transform: uppercase;
+      color: #f4f4f4;
+      text-decoration: none;
+      font-size: 20pt;
+      font-weight: 900;
+      margin-top: calc(25pt + 1vh);
+    }
   }
   .background {
     // border: 2px solid red;
@@ -74,6 +163,8 @@ export default {
     background-color: #5e8aa7;
     display: flex;
     overflow: hidden;
+    z-index: 5;
+    filter: blur(8.3px);
 
     .imgColumn {
       // border: 2px solid purple;
